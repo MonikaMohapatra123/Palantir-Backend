6ac97227b548f4122d69c2c3518754e6f29ae361
MonikaMohapatra <monikagudi123@gmail.com>
2025-12-18 12:45:37 +0530

    server.js change

--- server.js
+++ server.js
@@ -1,3 +1,30 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import pageRoutes from "./routes/pageRoutes.js";
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// mongoose
+//   .connect(process.env.MONGO_URL)
+//   .then(() => console.log("MongoDB Connected"))
+//   .catch(err => console.log(err));
+
+// app.use("/api/pages", pageRoutes);
+
+// app.listen(5000, () => {
+//   console.log("Server running on port 5000");
+// });
+
+
+
+
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -16,8 +43,12 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
+app.get("/", (req, res) => {
+  res.send("Palantir Backend API is Live 🚀");
+});
+
 app.use("/api/pages", pageRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+// ❌ DO NOT use app.listen()
+// export app instead
+export default app;
